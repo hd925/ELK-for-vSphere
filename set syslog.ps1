498e3c60ed16615ba732d698825865a09972d0c8
Ethan Huang <kuangmsn@gmail.com>
2015-05-15 11:10:12 +0800

    5.15

---
+++ set syslog.ps1
@@ -0,0 +1,12 @@
+Connect-VIServer vCenter Server  -user Username -password Password
+$VMHosts = Get-VMHost | Sort-Object Name
+$syslogserver = "tcp://Syslog Server IP"
+
+foreach ($VMHost in $VMHosts) {
+$FW = $VMhost | Get-VMHostFirewallException | Where {$_.Name -eq ��syslog��} | Set-VMHostFirewallException -Enabled $True
+Set-VMHostAdvancedConfiguration -Name Syslog.global.logHost -Value  $syslogserver  -VMHost $VMHost
+}
+Disconnect-VIServer -Confirm:$False
+
+
+
